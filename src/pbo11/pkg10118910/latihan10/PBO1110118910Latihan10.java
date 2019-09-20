1602655da38592cd8b1673dc20aac2a20c8bb54a
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:33:08 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan10/PBO1110118910Latihan10.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan10;
+
+/**
+ *
+ * @author
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaan Logika
+ */
+public class PBO1110118910Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+        
+    }
+    
+}
